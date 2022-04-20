05b7217bee3e2f8337834f2fca18799b88f1c7ca
myalpha95 <87795928+myalpha95@users.noreply.github.com>
2022-04-20 17:08:11 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from google.cloud import storage
+
+gcs_client = storage.Client(project='agr-sendirect-des')
+bucket = gcs_client.get_bucket('snd-bpdf')
+print(bucket)
+blob = bucket.blob('cliente2/')
+blob.upload_from_string('', content_type='application/x-www-form-urlencoded;charset=UTF-8')
