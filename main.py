a12cd7826665ce4b8c30086d3cae318449194dff
myalpha95 <87795928+myalpha95@users.noreply.github.com>
2022-04-20 17:11:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ bucket = gcs_client.get_bucket('snd-bpdf')
 print(bucket)
 blob = bucket.blob('cliente2/')
 blob.upload_from_string('', content_type='application/x-www-form-urlencoded;charset=UTF-8')
+
+def main():
+  password_id= "wqewqeewqqwe63226123321"
+  return password_id
